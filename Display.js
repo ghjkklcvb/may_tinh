18cd4e5cca1a879068cb3cc04027c41c3677d7ad
ghjkklcvb <165628949+ghjkklcvb@users.noreply.github.com>
2024-09-13 20:17:14 +0700

    Add files via upload

---
+++ Display.js
@@ -0,0 +1,12 @@
+// Display.js
+import React from 'react';
+
+const Display = ({ input }) => {
+  return (
+    <div className="display">
+      {input || '0'}
+    </div>
+  );
+};
+
+export default Display;
